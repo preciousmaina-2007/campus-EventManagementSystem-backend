9cacfd530064fc21327c5341a9c9aa203d5d9590
precious <precious.maina@student.moringaschool.com>
2026-07-21 11:15:21 +0300

    feat(setup): initialize Django project with custom user model

---
+++ accounts/models.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    pass
+
+class Role(models.TextChoices):
+        STUDENT = "STUDENT", "Student"
+        ORGANIZER = "ORGANIZER", "Organizer"
+        ADMIN = "ADMIN", "Admin"
+    
+role = models.CharField(
+    max_length=20,
+    choices=Role.choices,
+    default=Role.STUDENT,
+)
+
+def __str__(self):
+        return self.username
