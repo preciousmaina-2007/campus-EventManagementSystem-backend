9267eaba32be78f04b1f938749303f0d673b7f23
precious <precious.maina@student.moringaschool.com>
2026-07-21 11:41:28 +0300

    feat(accounts): add role field and customize user admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = (
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+        "role",
+        "is_staff",
+    )
+
+    list_filter = (
+        "role",
+        "is_staff",
+        "is_superuser",
+        "is_active",
+    )
+
+    search_fields = (
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+    )
--- accounts/models.py
+++ accounts/models.py
@@ -1,19 +1,18 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-class User(AbstractUser):
-    pass
 
-class Role(models.TextChoices):
+class User(AbstractUser):
+    class Role(models.TextChoices):
         STUDENT = "STUDENT", "Student"
         ORGANIZER = "ORGANIZER", "Organizer"
         ADMIN = "ADMIN", "Admin"
-    
-role = models.CharField(
-    max_length=20,
-    choices=Role.choices,
-    default=Role.STUDENT,
-)
 
-def __str__(self):
+    role = models.CharField(
+        max_length=20,
+        choices=Role.choices,
+        default=Role.STUDENT,
+    )
+
+    def __str__(self):
         return self.username
