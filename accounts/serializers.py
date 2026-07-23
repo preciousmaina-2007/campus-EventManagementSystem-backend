3d4b41f58b98e1e15e1c3d5d2a03a876685625f5
precious <precious.maina@student.moringaschool.com>
2026-07-23 17:28:08 +0300

    feat(auth): add custom JWT login serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from .models import User
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
 
 class UserRegistrationSerializer(serializers.ModelSerializer): 
     class Meta:
@@ -19,3 +21,23 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         
 def create(self, validated_data):
     return User.objects.create_user(**validated_data)        
+
+class LoginSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        return token
+
+def validate(self, attrs):
+    data = super().validate(attrs)
+
+    data["user"] = {
+        "id": self.user.id,
+        "username": self.user.username,
+        "email": self.user.email,
+        "first_name": self.user.first_name,
+        "last_name": self.user.last_name,
+        "role": self.user.role,
+    }
+
+    return data    
