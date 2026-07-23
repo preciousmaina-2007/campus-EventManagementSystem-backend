5f7ecb283deb1a9277e13c5d77cf9f467ed055aa
precious <precious.maina@student.moringaschool.com>
2026-07-23 18:12:39 +0300

    feat(auth): implement JWT authentication and user registration

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,43 +1,48 @@
 from rest_framework import serializers
-from .models import User
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
 
-class UserRegistrationSerializer(serializers.ModelSerializer): 
+from .models import User
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = User
         fields = [
-           "id",
-           "username",
-           "email",
-           "password",
-           "first_name",
-           "last_name",
-           "role",
-    ]
-    extra_kwargs = {
-    "password": {"write_only": True}
-}    
-        
-def create(self, validated_data):
-    return User.objects.create_user(**validated_data)        
+            "id",
+            "username",
+            "email",
+            "password",
+            "first_name",
+            "last_name",
+            "role",
+        ]
+        extra_kwargs = {
+            "password": {"write_only": True}
+        }
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
+
 
 class LoginSerializer(TokenObtainPairSerializer):
+
     @classmethod
     def get_token(cls, user):
         token = super().get_token(user)
         return token
 
-def validate(self, attrs):
-    data = super().validate(attrs)
+    def validate(self, attrs):
+        data = super().validate(attrs)
 
-    data["user"] = {
-        "id": self.user.id,
-        "username": self.user.username,
-        "email": self.user.email,
-        "first_name": self.user.first_name,
-        "last_name": self.user.last_name,
-        "role": self.user.role,
-    }
+        data["user"] = {
+            "id": self.user.id,
+            "username": self.user.username,
+            "email": self.user.email,
+            "first_name": self.user.first_name,
+            "last_name": self.user.last_name,
+            "role": self.user.role,
+        }
 
-    return data    
+        return data
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,14 @@
-from rest_framework import generics
+from rest_framework import generics,permissions
 from .models import User
-from .serializers import UserRegistrationSerializer
+from .serializers import UserRegistrationSerializer, LoginSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserRegistrationSerializer
+    permission_classes = [permissions.AllowAny]
+
+
+class LoginView(TokenObtainPairView):
+    serializer_class = LoginSerializer
+    permission_classes = [permissions.AllowAny]
