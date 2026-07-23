007368de125e8821f7aede0c562ec2e24efe0cc6
precious <precious.maina@student.moringaschool.com>
2026-07-23 14:52:54 +0300

    feat(auth): create user registration serializer

---
+++ accounts/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from .models import User
+
+class UserRegistrationSerializer(serializers.ModelSerializer): 
+    class Meta:
+        model = User
+        fields = [
+           "id",
+           "username",
+           "email",
+           "password",
+           "first_name",
+           "last_name",
+           "role",
+    ]
+    extra_kwargs = {
+    "password": {"write_only": True}
+}    
+        
+def create(self, validated_data):
+    return User.objects.create_user(**validated_data)        
